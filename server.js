7b98019b584e4a2587a548bfe47113f0f76670ec
Nikolaus Schuster <nikolausschuster@Nikolauss-MacBook-Pro.local>
2017-01-17 11:47:05 +0400

    add logging and make port configurable, fix finding of table

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var mysql = require("mysql")
 var http = require("http")
 
+var server_port = 3000
+if (typeof process.env.SERVER_PORT != "undefined") {
+  server_port = process.env.SERVER_PORT
+}
+
 function tryDb() {
   var con = mysql.createConnection({
     host: process.env.MYSQL_HOST,
@@ -13,10 +18,10 @@ function tryDb() {
     if(err){
       switch (err.code) {
         case "ER_BAD_DB_ERROR":
-          console.log("Unknown Database")
+          console.log(Date.now()+" Unknown Database: "+process.env.MYSQL_DB)
           break
         case "ECONNREFUSED":
-          console.log("Server not running yet")
+          console.log(Date.now()+" Server not running yet: "+process.env.MYSQL_HOST)
           break
         default:
           console.log(err)
@@ -25,17 +30,19 @@ function tryDb() {
       con.end();
       setTimeout(tryDb, 500)
     } else {
-      con.query('SHOW TABLES LIKE \''+process.env.MYSQL_TABLE+'\'', function(err, results, fields) {
+      console.log(Date.now()+" Connection established, database selected")
+      con.query("SELECT 1 FROM `"+process.env.MYSQL_TABLE+"` LIMIT 1", function(err, results, fields) {
         if (err) {
           console.log(err)
           con.end()
           setTimeout(tryDb, 500)
         } else {
-          console.log('Connection established')
+          console.log(Date.now()+' table found')
           var server = http.createServer(function(req, res) {
             res.write("NOOP").end()
           })
-          server.listen(3000)
+          server.listen(server_port)
+          console.log(Date.now()+" server listening on port "+server_port)
         }
       })
     }
